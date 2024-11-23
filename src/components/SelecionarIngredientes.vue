c814eeb2d5cc75850e1b17c6666445eac1fe20ba
尾花 <47906698+leonardoNael@users.noreply.github.com>
2024-11-23 10:53:23 -0300

    Aula 04| Listas dinâmicas

---
+++ src/components/SelecionarIngredientes.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { obterCategorias } from '@/http';
+
+    export default {
+        data() {
+            return {
+                categorias: obterCategorias()
+            }
+        }
+    }
+</script>
+
+<template>
+    <section class="selecionar-ingredientes">
+        <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+
+        <p class="paragrafo-lg instrucoes">Selecione abaixo os ingredientes que você quer usar nesta receita:</p>
+
+        <ul class="categorias">
+            <li v-for="categoria in categorias" :key="categoria.nome">
+                {{ categoria.nome }}
+            </li>
+        </ul>
+
+        <p class="paragrafo dica">
+            "Atenção: consideramos que você tem em casa sal, pimenta e água."
+        </p>
+    </section>
+</template>
